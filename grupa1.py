954b10dd64c37f831aca1817da4e1c55885e0d6f
unknown <sarasaravevic@gmail.com>
2022-03-12 01:52:44 +0100

    commit

---
+++ grupa1.py
@@ -0,0 +1,179 @@
+ # broj="sara" #ovde imamo varijablu i string
+# print(broj)
+# broj=input("unesite zeljeni broj")
+# print(broj)
+# OTORINOLARINGOLOGIJA LOGIJA
+
+# 1638 int 
+# broj=7585497
+# print(broj)
+# 8183.986 float
+# broj=7846
+# "khdhueb"
+# "5885685"
+# ime="bdhwjdvnejcb"
+# print(ime)
+# broj=678
+# print(broj)
+# broj="sarsaracevic"
+# print(len(broj))
+# a = 33
+# b = 200
+# if a >b:
+#     print("a je od b")
+# else:
+#     print("a nije vece od b")
+# rint(10>5)p
+# a=7
+# print(bool(a))
+# a=0
+# print(bool(a))
+
+# x=int(input("unesite neki broj"))
+# y=int(input("unesite neki broj"))
+# if x<y:
+#     print("x je manje y")
+# else:
+#     print('x je vece y')
+
+#IF ELIF ELSE
+#GRADI SE NA OSNOVU TRUE I FALSE ,JER BIRAMO IZMEDJU DVA PONUDJENA ODGOVORA
+# 
+# a>b lepo je vreme napolju
+# a<b lose je vreme napolju
+# kontra nije lose vreme napolju
+
+
+
+# x=int(input("unesite neki broj:"))
+# y=int(input("unesite neki broj:"))
+# if x>y:
+#     print("lepo je vreme danas")
+# elif x<y:
+#     print("lose je vreme danas")
+# else:
+#     print("nije lose vreme napolju")
+# x=int(input("unesite neki broj"))
+# y=int(input("unesite neki broj"))
+# z=x+y #postavljamo zadatak
+# if z>20:#koristimo dostupne operatore za if 
+#     print("suma je veca od 20")
+# else:
+#     print("suma je manja od 20")
+
+
+# x=int(input("unesite random broj"))
+# y=int(input("unesite random broj"))
+# z=x+y
+# s=x*y  #mozemo printati varijablu s i z da bi videli zbir i proizvod
+# if z==s:
+#     print("zbir i proizvod su jednaki")
+# else:
+#     print( "zbir i proizvod su razliciti")
+
+# a = int(input("unesite neki broj"))
+# b = int(input("unesite neki broj"))
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+
+# x=int(input("unesite neki broj od 1 do 12"))
+# if x==1:
+#     print("januar")
+# elif x==2:
+#     print("februar")
+
+
+# f and f => f
+# f and t => f
+# t and f => f
+# t and t => t
+                     #logicki operatori i tjst. ili
+# f or f => f
+# f or t => t
+# t or f => t
+# t or t => t
+
+
+# a =int(input("Unesi neki broj izmedju 1 i 12 :"))
+# if a==1:
+#     print("Januar")
+# elif a==2:
+#     print("Februar")
+# elif a==3:
+#     print("Mart")
+# elif a==4:
+#     print("April")
+# elif a==5:
+#     print("Maj")
+# elif a==6:
+#     print("Jun")
+# elif a==7:
+#     print("Jul")
+# elif a==8:
+#     print("Avgust")
+# elif a==9:
+#     print("Septembar")
+# elif a==10:
+#     print("Oktobar")
+# elif a==11:
+#     print("Novembar")
+# elif a==12:
+#     print("Decembar")
+# else:
+#     print("Izlaz iz Programa")
+# ....-4 -3 -2 -1 0 1 2 3 4...
+
+# x=int(input("unesite neki broj"))
+# if x>0:
+#     print("pozitivan")
+# else:
+#     print("negativan")
+
+# x=input("unesite neku rec")
+# if "s" in x:
+#     print("slovo  s  se nalazi u ovoj reci")
+# else:
+#     print("slovo s  se ne nalazi u ovoj reci")
+
+# x="informatika"
+# if "f" in x:
+#     print("slovo f se nalazi")
+# else:
+#     print("slovo f se ne nalazi")
+
+
+# ime = input("Kako se zove tip podatka koji uvek pisemo u zagradama: ")
+# if ime=="string":   
+#     print("tacan odgovor")
+# else:
+#      print("Nažalost nisi pogodila :(")
+
+# a=int(input('unesite stranicu'))
+# b=int(input('unesite stanicu'))
+# if a==b:
+#     print('povrsina kvadrata iznosi',a**2)#ako su dva ista broja npr 5*5 mozemo zapisati kao 5 na kvadrat(5**2)
+# else:
+#     print('povrsina provogaonika iznosi',a*b)
+
+
+# h=str(input("odaberite operaciju:"))
+# if h=="zbir":
+#     a=int(input("unesite neki broj"))
+#     b=int(input("unesite drugi broj"))
+#     print("zbir je;", a+b)
+# elif h=="razlika":
+#     a=int(input("unesite neki broj"))
+#     b=int(input("unesite drugi broj"))
+#     print("razlika je:", a-b)
+# elif h=="proizvod":
+#     a=int(input("unesite neki broj"))
+#     b=int(input("unesite drugi broj"))
+#     print("proizvod je:" , a*b)
+# elif h=="kolicnik":
+#     a=int(input("unesite neki broj"))
+#     b=int(input("unesite drugi broj"))
+#     print("kolicnik je:", a/b)
+# else:
+#     print("kraj")
