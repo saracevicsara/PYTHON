ddcbc28672a4b8ccebddea2dfeb43ac358f249c9
unknown <sarasaravevic@gmail.com>
2022-05-30 21:00:50 +0200

    .

---
+++ grupa2.py
@@ -0,0 +1,10 @@
+# typing.com/join#6291050646E3D
+# int-5,9865,7,-4,0,987654323456789087654567,98765432
+# folat-5,7;1,2;345678,987654;-345678,987654;
+# string-"sara","56","9988,78","true","false","34567890iuygfddfghj8uh54ertyu89-098765r4ewsdfghu765trfcvbhu7654e"
+# 'sara',"345678","""zxsdfghjk """,'''dfghj'''
+# bool-True,False
+# ime="sara"
+# print(ime)
+print("sara")
+print("sara")
