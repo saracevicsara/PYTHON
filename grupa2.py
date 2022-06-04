1d9c5fd545e04f38bcf44563df30a7e016ae6779
unknown <sarasaravevic@gmail.com>
2022-06-04 13:02:19 +0200

    commit

--- grupa2.py
+++ grupa2.py
@@ -6,5 +6,31 @@
 # bool-True,False
 # ime="sara"
 # print(ime)
-print("sara")
-print("sara")
+# print("sara")
+# print("sara")
+# "dbucbsio","124",".,_@!"
+# 2,5,7,87,999,9876543 integer-int
+# 7,1;8,9;45,4;-folat
+# True
+# False
+# broj=5
+# ime="sara"
+# decimalniBroj=3,5
+# bool=True
+# print(bool)
+# print(decimalniBroj)
+# print(ime)
+# mojBrojGodina=15
+# print(mojBrojGodina)
+# prezime="saracevic"
+# print(prezime)
+# ime=input("unesite svoje ime:")
+# print(ime)
+# ime="sara"
+# prezime="Saracevic"
+# print(ime+prezime)
+# ime=input("unesite svoje ime:")
+# prezime=input("unesite svoje prezime:")
+# print(ime+prezime)
+#spojite ime ,prezime i godine u jednu recenicu
+
