dd575737c380438b1ab6b35e5d5c021f374b4ccb
unknown <sarasaravevic@gmail.com>
2022-06-12 13:24:52 +0200

    commit

---
+++ vezbanje2.py
@@ -0,0 +1,37 @@
+#varijabla je memoriska lokacija ,koja se koristi za cuvanje podataka
+
+
+#OSNOVNI TIPOVI PODATAKA
+# 5-int-ceo broj
+# "jeinhor"-niz karaktera pod navodnicama
+# 1.9-float-decimalni broj
+# True-bool-logicki broj
+# False-bool-logicki broj
+# 0-False
+# 1-True
+# 01000100011101110-binarni broj
+
+#SLOZENI TIPOVI PODATAKA
+# NASTAJU KOMBINOVANJEM OSNOVNIH TIPOVA PODATAKA
+#-----------------------------------------------------------------------
+# broj=4
+# print(broj+5)
+#-----------------------------------------------------------
+
+
+# broj=int(input("unesite neki ceo broj"))
+# print(broj)
+# 3.5-jabuke
+# 4-tresanja
+# 5-kruska
+# jabuke=3.5
+# tresanja=4
+# kruska=5
+# print(jabuke+tresanja+kruska)
+# jabuke=3.5
+# tresanja=4
+# kruska=5
+# suma=jabuke+tresanja+kruska
+# print(suma)
+a=str(input("unesite neku rec:"))
+print(a*5)
