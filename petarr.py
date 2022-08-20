d2419128954855a259400f0ef83b8e8580f6203e
unknown <sarasaravevic@gmail.com>
2022-08-20 14:55:03 +0200

    nst

---
+++ petarr.py
@@ -0,0 +1,146 @@
+
+# dict = {
+#     "ime" : "Petar",
+#     "godine" : 17,
+#     "skola" : True,
+# }
+
+# print(dict)
+# print(dict["ime"])
+# print(dict["skola"])
+
+# for i in dict:
+#     print(dict[i])
+
+
+# for i in dict.values():
+#     print(i)
+
+# print(100 * "=")
+
+# for i in dict.keys():
+#     print(i)
+
+# print(100 * "=")
+
+# for i,j in dict.items():
+#     print(i,j)
+
+
+# ----------------------------------
+
+# recnik = {
+#     "ime" : "Petar",
+#     "godine" : 17,
+#     "skola" : True,
+#     "zanrovi" : ["rock", "pop", "hip-hop"],
+# }
+
+# # recnik["grad"] = "Novi Pazar"
+
+
+# # recnik["ime"] = "Petar Petrovic"
+
+# # print(recnik)
+
+# recnik.update({"vreme" : "nocno"})
+
+# print(recnik)
+
+# ---------------------------------
+
+# recnik = dict()
+
+# n = int(input("Unesite broj: "))
+
+# for i in range(1, n+1):
+#     recnik[i] = i**2
+
+# print(recnik)
+
+# -----------------------------
+
+# sampleDict = {
+#     "class" : {
+#         "students" : {
+#             "name" : "Petar",
+#             "marks" : {
+#                 "math" : 70,
+#                 "history" : 80,
+#             }
+#         }
+#     }
+# }
+
+
+# print(sampleDict["class"]["students"]["marks"]["history"])
+
+
+# ----------------
+
+
+dict = {
+    "emp1" : {
+        "name" : "Petar", "salary" : 500
+    },
+    "emp2" : {
+        "name" : "Marija", "salary" : 600
+    },
+    "emp3" : {
+        "name" : "Ana", "salary" : 700
+    }
+}
+
+for i in dict.values():
+    if i["name"] == "Marija":
+        i["salary"] = 1000
+        break
+print(dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
