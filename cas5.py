330f9696ea8623d53c0abd14e119c5a139ef60d8
unknown <sarasaravevic@gmail.com>
2022-10-29 11:11:10 +0200

    if else statment

---
+++ cas5.py
@@ -0,0 +1,16 @@
+# imevarijable=  #potpuna varijabla
+# ==,!=#uporedjivanje
+# +#operator sabiranja kod brojeva i operator nadodavnja kod str
+# -#operator oduzimanja\
+#>,<,>=,<=
+# #% proverava ostatak pri deljenju
+# print()
+# input()#zahteva definisanje podataka int,str,float
+# if uslov :
+    # print()
+# elif uslov:
+#     print()
+# else: #kontra uslov
+#     print()
+#w3schools/python/
+
---
+++ jana.py
@@ -0,0 +1,45 @@
+# string ""''""""""
+# 20 30 7 
+# 9.9
+# True False
+
+# print 
+# input 
+
+
+# ime =str(input("unesite svoje ime"))
+# print(ime)
+
+# ime=str(input("unesite svoje ime"))
+# prezime=str(input("unesite svoje prezime"))
+# print(ime + prezime)
+
+
+# broj =5
+# if broj>7:
+#     print("broj je veci od 7")
+# else:
+#     print("broj je manji od 7")
+
+
+# broj =int(input("unesite neki broj"))
+# if broj==20:
+#     print('broj je jedank 20')
+# else: #kontra od if-a
+#     print('broj nije jednak 20')
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
