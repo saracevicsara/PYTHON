fbc29ed8fba914112026cbcb89d464b698ac3de2
unknown <sarasaravevic@gmail.com>
2022-11-06 14:32:26 +0100

    petar

---
+++ drugi ciklus/petar.py
@@ -0,0 +1,114 @@
+
+
+# print znaci ispis ili stampaj i to se odnsoi na terminal
+# print("ovo je kurs pajtona")
+# print("caooom svimA")
+
+
+# moja_skola = "gimnazija"
+# broj = 5854556
+
+# print(broj)
+
+# broj = 5
+# drugi_broj = 6
+
+# print(broj + drugi_broj)
+# print(broj - drugi_broj)
+#     #    5    *     6
+# print(broj * drugi_broj)
+
+# ------------------------------------------
+
+# string, str
+# varijabla = "dxastxfsctxtas8959859@#@#!@!##@!"
+# var1 = "sacgugcyugcvsdvv"
+# var2 = "421"
+# # integer, int, celi broj
+# brojJedan = 5
+# broj3 = -65899
+
+# # float
+# brojDva = 6.54
+
+# ime = "Said"  
+# godina = 18
+# print("Moje ime je " + ime)
+# print("Ja imam", godina, "bhabcsuhyasbchgascuyusa")
+
+
+# ime = "Petar"
+# prezime = "Vucinic"
+
+# print("Moje ime je", ime, "a moje prezime je", prezime)
+# print("Moje ime je " + ime + " a moje prezime je " + prezime)
+
+
+# ja imam --- godina i idem u ------- skolu.
+
+# skola = "gimnazija"
+# godina = 17
+
+# print("Ja imam", godina, "godina i idem u", skola, "skolu")
+# print("Ja imam " , godina , " godina i idiem u " + skola + " skolu")
+
+# --------------------
+ 
+# broj = int(input("unesite neki broj: "))
+# print("uneti broj je", broj)
+
+# --------------------
+
+# ime = str(input("Unesite vase ime: "))
+# print("Moje ime je", ime)
+
+# --------------------
+
+
+# brojJedan = int(input("Unesite neki broj: "))
+# brojDva = int(input("Unesite neki broj: "))
+
+# print("Zbir ova dva broja je", brojJedan+brojDva)
+
+
+# ------------------------
+
+# broj1 = int(input("unesite prvi broj: "))
+# broj2 = int(input("unesite drugi broj: "))
+# broj3 = int(input("unesite treci broj: "))
+
+# vrednost_izraza = (broj1+broj2)/broj3
+
+# # print((broj1+broj2)/broj3)
+# print(vrednost_izraza)
+
+
+# ------------------------------
+
+# a = float(input("iUnesite duzinu stranica a: "))
+# b = float(input("iUnesite duzinu stranica b: "))
+
+# povrsina = a*b
+# print(povrsina)
+
+# ---------------------------
+
+
+eur = int(input("Unesite koliko imate evra: "))
+print("To je", eur * 117.5, "dinara")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
