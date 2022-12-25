11bfcc3b143ec465528ba056b40fe2c885d7a84b
unknown <sarasaravevic@gmail.com>
2022-12-25 14:00:10 +0100

    turtle

--- drugi ciklus/turtlle.py
+++ drugi ciklus/turtlle.py
@@ -1,11 +1,43 @@
-from turtle import *
-color('red', 'yellow')
-begin_fill()
-while True:
-    forward(200)
-    left(170)
-    if abs(pos()) < 0:
-        break
-end_fill()
-done()
+# from turtle import *
+# color('red', 'yellow')
+# begin_fill()
+# while True:
+#     forward(200)
+#     left(170)
+#     if abs(pos()) < 0:
+#         break
+# end_fill()
+# done()
     
+
+# from turtle import *
+# color('blue')
+# width(5)
+# begin_fill()
+# for i in range(3):
+#     forward(100)
+#     left(120)
+# end_fill()
+
+# from turtle import *
+# color('red')
+# width(5)
+# begin_fill()
+# for i in range(4):
+#     forward(200)
+#     left(90)
+# end_fill()
+
+from turtle import *
+color('blue')
+width(5)
+begin_fill()
+for i in range(3):
+    forward(100)
+    left(120)
+rt(90)
+color("red")
+for i in range(4):
+     forward(100)
+     left(90)
+end_fill()
