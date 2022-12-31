ed0a3ffc5395f7264fcc04358dc8d49ab4d75c93
unknown <sarasaravevic@gmail.com>
2022-12-31 13:46:56 +0100

    turtle

--- drugi ciklus/turtlle.py
+++ drugi ciklus/turtlle.py
@@ -27,17 +27,38 @@
 #     forward(200)
 #     left(90)
 # end_fill()
-
-from turtle import *
-color('blue')
-width(5)
-begin_fill()
-for i in range(3):
-    forward(100)
-    left(120)
-rt(90)
-color("red")
-for i in range(4):
-     forward(100)
-     left(90)
-end_fill()
+#kucica sa vratima
+# from turtle import *
+# color('blue')
+# width(5)
+# begin_fill()
+# for i in range(3):
+#     forward(100)
+#     left(120)
+# rt(90)
+# for i in range(4):
+#      forward(100)
+#      left(90)
+# forward(100)
+# left(90)
+# forward(35)
+# left(90)
+# forward(40)
+# right(90)
+# forward(30)
+# right(90)
+# forward(40)
+# done()
+# from turtle import*
+# for i in range(4):
+#      forward(100)
+#      left(90)
+# pu()
+# fd(5)
+# lt(90)
+# fd(5)
+# rt(90)
+# pd()
+# for i in range(4):
+#     forward(90)
+#     left(90)
