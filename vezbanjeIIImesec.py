e9a4abba4329426e6255c3831450760d27ade934
sarsi <sarasaravevic@gmail.com>
2023-03-04 14:10:16 +0100

    vezbanje

---
+++ vezbanjeIIImesec.py
@@ -0,0 +1,30 @@
+# ali vapljanin:2
+# ajlana:2
+# amir:1
+# hamza:2
+#kadir:2-
+#ali:2+
+#amar:1
+#sara:3
+
+
+# ime="sara"
+# # print(type(ime))
+# ime="sara2"
+# print(ime)
+# print(len(ime))
+# broj=2
+# drugi_broj=8
+# print(broj+drugi_broj)
+# ime="Ajlana"
+# print("ja se zovem "+ime)
+# skola=str(input("u koju skolu idete"))
+# print("ja idem u:"+skola)
+# ime=str(input("unesite svoje ime"))
+# prezime=str(input("unesite svoje prezime"))
+# godine=int(input("unesite svoje godine"))
+# print("ja se zovem "+ime,"prezivam se "+prezime,"imam",godine)
+
+
+# korisnik=int(input("unesite neki ceo broj"))
+# print(float(korisnik)+1.53)
