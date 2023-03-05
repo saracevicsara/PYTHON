fece05feccf9f08fb9895b29339b581da3767882
sarsi <sarasaravevic@gmail.com>
2023-03-05 14:02:24 +0100

    vezbanje

--- vezbanjeIIImesec.py
+++ vezbanjeIIImesec.py
@@ -7,7 +7,36 @@
 #ali:2+
 #amar:1
 #sara:3
+#lejla:3
+#adem:2+
+#isa:3
+#mehmed:1
 #----------------------------------------
+#prvi ciklus petlje i if(1-3)
+#ali vapljanin:1
+#ajlana:2+
+#hamza:1
+#kadir:3
+#amar:1
+#lejla:2
+#adem:3
+#isa:3
+#mehmed:2
+#----------------------------------
+# a=2
+# b=3
+# if a<b:
+#     print("a je manje od b")
+# a=3
+# b=2
+# if a+b==5:
+#     print("bravo")
+# a=7
+# if a>10:
+#     print("vreme napolju je umereno toplo")
+# a=12
+# if a>15:
+#     print("karta je 200 din")
 
 # ime="sara"
 # # print(type(ime))
@@ -29,3 +58,12 @@
 
 # korisnik=int(input("unesite neki ceo broj"))
 # print(float(korisnik)+1.53)
+# ime="Lejla"
+# print(ime)
+# korisnik=int(input("unesite dinare"))
+# print(korisnik/117)
+# korisnik=int(input("unesite eure"))
+# euro=117
+# dinari=korisnik*euro
+# print(dinari)
+
